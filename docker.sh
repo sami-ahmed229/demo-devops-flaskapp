7fe8ea5dfc37e0bd45722ee9fb5c53055b115e54
sami-ahmed229 <mohammedsami99852@gmail.com>
2025-09-17 19:14:48 +0530

    Delete docker.sh

--- docker.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
-	  -u root \
-	    --name jenkins \
-	      -p 8080:8080 -p 50000:50000 \
-	        -v /mnt/c/Users/DELL/Documents/jenkins_home:/var/jenkins_home \
-		  -v /var/run/docker.sock:/var/run/docker.sock \
-		    jenkins/jenkins:jdk21
-
