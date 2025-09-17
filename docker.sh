75f3b74ce5926661e155f073beaac2866ca8718f
sami-ahmed229 <mohammedsami99852@gmail.com>
2025-09-17 19:05:23 +0530

    final changes

--- docker.sh
+++ docker.sh
@@ -1,8 +1,8 @@
 docker run -d \
-	  --name jenkins \
-	    -p 8080:8080 -p 50000:50000 \
-	      -v jenkins_home:/var/jenkins_home \
-	        -v /var/run/docker.sock:/var/run/docker.sock \
-		  jenkins/jenkins:jdk21 \
-		  
+	  -u root \
+	    --name jenkins \
+	      -p 8080:8080 -p 50000:50000 \
+	        -v /mnt/c/Users/DELL/Documents/jenkins_home:/var/jenkins_home \
+		  -v /var/run/docker.sock:/var/run/docker.sock \
+		    jenkins/jenkins:jdk21
 
